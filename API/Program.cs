db51ed22fad76d0a0492ddd0a9d58cff1dac4913
Stoyan Penchev <stoianpp2013@gmail.com>
2023-01-06 17:17:14 +0200

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,55 @@
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddControllers();
+var str = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<StoreContext>(x =>
+    x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+});
+
+var host = builder.Build();
+using var scope = host.Services.CreateScope();
+var services = scope.ServiceProvider;
+var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+try
+{
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context, loggerFactory);
+}
+catch (Exception ex)
+{
+    var logger = loggerFactory.CreateLogger<Program>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
+// Configure the HTTP request pipeline.
+if (host.Environment.IsDevelopment())
+{
+    host.UseSwagger();
+    host.UseSwaggerUI();
+}
+
+host.UseHttpsRedirection();
+
+host.UseAuthorization();
+
+host.MapControllers();
+
+host.Run();
